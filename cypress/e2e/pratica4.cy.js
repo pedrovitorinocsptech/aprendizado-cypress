46ef46f0192accf436f496e8ed3dd06f409d407d
Pedro Silva <pedro.silva@csptech.com.br>
2025-03-13 17:17:32 -0300

    Automação sextou recife

--- cypress/e2e/pratica4.cy.js
+++ cypress/e2e/pratica4.cy.js
@@ -1,5 +1,14 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://flappybird.io/')
+    cy.visit('https://sextourecife.com.br/')
+    cy.get('.texto-mapa').click()
+    cy.get('.navbar-map-page > :nth-child(2)').click()
+    cy.get('#categorias').select('Festa')
+    cy.get('p > :nth-child(4)').click()
+    cy.get('#input-nome').type('Pedro Silva')
+    cy.get('.endereco > input').type('pedro@email.com')
+    cy.get('select').select('Bugs')
+    cy.get('textarea').type('O botão do header não tem classe')
+    cy.get('.botao2').click()
   })
 })
